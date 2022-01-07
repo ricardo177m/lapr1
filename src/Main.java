92cdc56d853c6742388a37b4410920af6c07ea29
Ricardo Moreira <1211285@isep.ipp.pt>
2022-01-07 12:10:13 +0000

    análise diária

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // 2021-2022 LEI, Turma 1DI
 //
 // Realizado por:
-// André Barros(1211299)
-// Tomás Russo(1211288)
-// João Caseiro(1211334)
-// Ricardo Moreira(1211285)
+// André Barros - 1211299
+// Tomás Russo - 1211288
+// João Caseiro - 1211334
+// Ricardo Moreira - 1211285
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,7 +19,7 @@ import java.util.Scanner;
 public class Main {
     static final Scanner kbScanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, ParseException {
         ArrayList<String> datas = new ArrayList<String>();
         ArrayList<Integer> acumuladoNaoInfetados = new ArrayList<Integer>();
         ArrayList<Integer> acumuladoInfetados = new ArrayList<Integer>();
@@ -31,7 +31,6 @@ public class Main {
         // Início do programa
         do {
             selecaoUtilizador = menu();
-
             System.out.println();
 
             switch (selecaoUtilizador) {
@@ -39,16 +38,15 @@ public class Main {
                     lerFicheiro(selecionarFicheiro(), datas, acumuladoNaoInfetados, acumuladoInfetados,
                             acumuladoHospitalizados,
                             acumuladoUCI, acumuladoMortes);
-
-                    for (int i = 0; i < datas.size(); i++) {
-                        System.out.println(acumuladoMortes.get(i));
-                    }
-
-                    pressioneEnterParaCont();
                     break;
 
                 case 2:
+                    mostrarDadosDiarios(leituraDeDatas(), datas, acumuladoInfetados, acumuladoHospitalizados,
+                            acumuladoUCI, acumuladoMortes);
+                    pressioneEnterParaCont();
+                    break;
                 case 3:
+                    mostrarDadosSemanais(leituraDeDatas(), datas);
                 case 4:
                 case 5:
                     System.out.println("Não implementado.");
@@ -58,7 +56,6 @@ public class Main {
                 case 0:
                     // terminar a execução do programa
                     break;
-
                 default:
                     System.out.println("Opção inválida.");
                     pressioneEnterParaCont();
@@ -92,8 +89,9 @@ public class Main {
     }
 
     public static void lerFicheiro(String filePath, ArrayList<String> datas, ArrayList<Integer> acumuladoNaoInfetados,
-            ArrayList<Integer> acumuladoInfetados, ArrayList<Integer> acumuladoHospitalizados,
-            ArrayList<Integer> acumuladoUCI, ArrayList<Integer> acumuladoMortes) throws FileNotFoundException {
+                                   ArrayList<Integer> acumuladoInfetados, ArrayList<Integer> acumuladoHospitalizados,
+                                   ArrayList<Integer> acumuladoUCI, ArrayList<Integer> acumuladoMortes) throws FileNotFoundException {
+
         Scanner fileScanner = new Scanner(new File(filePath));
         String linha = fileScanner.nextLine();
         String[] dados;
@@ -113,6 +111,84 @@ public class Main {
     public static String selecionarFicheiro() {
         System.out.print("Insira o caminho absoluto do ficheiro: ");
         return kbScanner.nextLine();
+    }
+
+    public static String[] leituraDeDatas() throws ParseException {
+        String[] leituraDatas = new String[2];
+        boolean verificado;
+
+        do {
+            System.out.println("Insira a data inicial:");
+            leituraDatas[0] = kbScanner.nextLine();
+            System.out.println("Insira a data final:");
+            leituraDatas[1] = kbScanner.nextLine();
+            verificado = verificarDatas(leituraDatas[0], leituraDatas[1]);
+
+            if (!verificado)
+                System.out.println("\nErro: datas inválidas!\n");
+        } while (!verificado);
+
+        return leituraDatas;
+    }
+
+    public static void mostrarDadosDiarios(String[] leituraDeDatas, ArrayList<String> datas,
+                                           ArrayList<Integer> acumuladoInfetados, ArrayList<Integer> acumuladoHospitalizados,
+                                           ArrayList<Integer> acumuladoUCI, ArrayList<Integer> acumuladoMortes) {
+
+        int indexData1 = datas.indexOf(leituraDeDatas[0]);
+        int indexData2 = datas.indexOf(leituraDeDatas[1]);
+
+        System.out.printf("Data %15s Novos Infetados | Novas Hospitalizações | Novos UCI | Novas Mortes\n", "");
+        for (int i = 0; i <= indexData2 - indexData1; i++) {
+            int novosInfetados = dadosDiariosNovos(acumuladoInfetados, leituraDeDatas, datas).get(i);
+            int novosHospitalizacoes = dadosDiariosNovos(acumuladoHospitalizados, leituraDeDatas, datas).get(i);
+            int novosUCI = dadosDiariosNovos(acumuladoUCI, leituraDeDatas, datas).get(i);
+            int novosMortes = dadosDiariosNovos(acumuladoMortes, leituraDeDatas, datas).get(i);
+            System.out.printf("%s %25s | %21.10s | %9.10s | %12.10s \n", datas.get(i + indexData1), novosInfetados,
+                    novosHospitalizacoes, novosUCI, novosMortes);
+        }
+    }
+
+    public static ArrayList<Integer> dadosDiariosNovos(ArrayList<Integer> dados, String[] leituraDeDatas,
+                                                       ArrayList<String> datas) {
+        ArrayList<Integer> dadosNovos = new ArrayList<>();
+        int indexData1 = datas.indexOf(leituraDeDatas[0]);
+        int indexData2 = datas.indexOf(leituraDeDatas[1]);
+        for (int i = indexData1; i <= indexData2; i++) {
+            dadosNovos.add(i - 1 < 0 ? 0 : dados.get(i) - dados.get(i - 1));
+        }
+        return dadosNovos;
+    }
+
+    public static void mostrarDadosSemanais(String[] leituraDeDatas, ArrayList<String> datas) throws ParseException {
+        // mostrar dados das semanas entre as datas pretendidas, ou seja, por exemplo:
+        // dados da 1 semn, dados da 2 semn, dados da 3 semn
+        // 2020-04-01 2020-04-02 -> NÃ£o Ã© uma semana completa
+        // 2020-04-1 2020-04-17 -> mostra 2 semanas 1-15
+        // 1 semana, subtrair acumulado do ult dia semana com 1 dia semn
+
+        if (verificarSemana(leituraDeDatas, datas)) {
+            System.out.printf("%s %25s | %21.10s | %9.10s | %12.10s \n");
+        } else {
+            do {
+                System.out.println("Introduza datas que contenham pelo menos 1 semana");
+                leituraDeDatas();
+                verificarSemana(leituraDeDatas, datas);
+            } while (!verificarSemana(leituraDeDatas, datas));
+        }
+    }
+
+    public static boolean verificarSemana(String[] leituraDeDatas, ArrayList<String> datas) {
+        boolean verificado = false;
+        int indexData1 = datas.indexOf(leituraDeDatas[0]);
+        int indexData2 = datas.indexOf(leituraDeDatas[1]);
+        if (((indexData2 + 1) - (indexData1 - 1)) % 6 == 0) {
+            verificado = true;
+        }
+        return verificado;
+    }
+
+    public static void mostrarDadosMensais() {
     }
 
     public static boolean verificarDatas(String data1, String data2) throws ParseException {
